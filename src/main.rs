6d1ff949eea25577196fb68a62c446b5523c2031
John Carmack <johncarmack@me.com>
2024-04-21 15:04:20 -0700

    Add post CRUD operations and DB migrations

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,126 @@
-//! This is an example function that leverages the Lambda Rust runtime HTTP support
-//! and the [axum](https://docs.rs/axum/latest/axum/index.html) web framework.  The
-//! runtime HTTP support is backed by the [tower::Service](https://docs.rs/tower-service/0.3.2/tower_service/trait.Service.html)
-//! trait.  Axum's applications are also backed by the `tower::Service` trait.  That means
-//! that it is fairly easy to build an Axum application and pass the resulting `Service`
-//! implementation to the Lambda runtime to run as a Lambda function.  By using Axum instead
-//! of a basic `tower::Service` you get web framework niceties like routing, request component
-//! extraction, validation, etc.
-use axum::extract::Query;
-use axum::http::StatusCode;
 use axum::{
-    extract::Path,
+    extract::{Path, State},
     response::Json,
-    routing::{get, post},
+    routing::get,
     Router,
 };
-use lambda_http::{run, tracing, Error};
+use bb8::Pool;
+use diesel::prelude::*;
+use diesel_async::{
+    pooled_connection::AsyncDieselConnectionManager, AsyncPgConnection, RunQueryDsl,
+};
+use lambda_http::{http::StatusCode, run, tracing, Error};
 use serde::{Deserialize, Serialize};
-use serde_json::{json, Value};
-use std::env::set_var;
 
-#[derive(Deserialize, Serialize)]
-struct Params {
-    first: Option<String>,
-    second: Option<String>,
-}
-
-async fn root() -> Json<Value> {
-    Json(json!({ "msg": "I am GET /" }))
-}
-
-async fn get_foo() -> Json<Value> {
-    Json(json!({ "msg": "I am GET /foo" }))
-}
-
-async fn post_foo() -> Json<Value> {
-    Json(json!({ "msg": "I am POST /foo" }))
-}
-
-async fn post_foo_name(Path(name): Path<String>) -> Json<Value> {
-    Json(json!({ "msg": format!("I am POST /foo/:name, name={name}") }))
-}
-
-async fn get_parameters(Query(params): Query<Params>) -> Json<Value> {
-    Json(json!({ "request parameters": params }))
-}
-
-/// Example on how to return status codes and data from an Axum function
-async fn health_check() -> (StatusCode, String) {
-    let health = true;
-    match health {
-        true => (StatusCode::OK, "Healthy!".to_string()),
-        false => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Not healthy!".to_string(),
-        ),
+table! {
+    posts (id) {
+        id -> Integer,
+        title -> Text,
+        content -> Text,
+        published -> Bool,
     }
+}
+
+#[derive(Default, Queryable, Selectable, Serialize)]
+struct Post {
+    id: i32,
+    title: String,
+    content: String,
+    published: bool,
+}
+
+#[derive(Deserialize, Insertable)]
+#[diesel(table_name = posts)]
+struct NewPost {
+    title: String,
+    content: String,
+    published: bool,
+}
+
+type AsyncPool = Pool<AsyncDieselConnectionManager<AsyncPgConnection>>;
+type ServerError = (StatusCode, String);
+
+async fn create_post(
+    State(pool): State<AsyncPool>,
+    Json(post): Json<NewPost>,
+) -> Result<Json<Post>, ServerError> {
+    let mut conn = pool.get().await.map_err(internal_server_error)?;
+
+    let post = diesel::insert_into(posts::table)
+        .values(post)
+        .returning(Post::as_returning())
+        .get_result(&mut conn)
+        .await
+        .map_err(internal_server_error)?;
+
+    Ok(Json(post))
+}
+
+async fn list_posts(State(pool): State<AsyncPool>) -> Result<Json<Vec<Post>>, ServerError> {
+    let mut conn = pool.get().await.map_err(internal_server_error)?;
+
+    let posts = posts::table
+        .filter(posts::dsl::published.eq(true))
+        .load(&mut conn)
+        .await
+        .map_err(internal_server_error)?;
+
+    Ok(Json(posts))
+}
+
+async fn get_post(
+    State(pool): State<AsyncPool>,
+    Path(post_id): Path<i32>,
+) -> Result<Json<Post>, ServerError> {
+    let mut conn = pool.get().await.map_err(internal_server_error)?;
+
+    let post = posts::table
+        .find(post_id)
+        .first(&mut conn)
+        .await
+        .map_err(internal_server_error)?;
+
+    Ok(Json(post))
+}
+
+async fn delete_post(
+    State(pool): State<AsyncPool>,
+    Path(post_id): Path<i32>,
+) -> Result<(), ServerError> {
+    let mut conn = pool.get().await.map_err(internal_server_error)?;
+
+    diesel::delete(posts::table.find(post_id))
+        .execute(&mut conn)
+        .await
+        .map_err(internal_server_error)?;
+
+    Ok(())
+}
+
+fn internal_server_error<E: std::error::Error>(err: E) -> ServerError {
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // If you use API Gateway stages, the Rust Runtime will include the stage name
-    // as part of the path that your application receives.
-    // Setting the following environment variable, you can remove the stage from the path.
-    // This variable only applies to API Gateway stages,
-    // you can remove it if you don't use them.
-    // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
-    set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
-
     // required to enable CloudWatch error logging by the runtime
     tracing::init_default_subscriber();
 
+    // Set up the database connection
+    let db_url = std::env::var("DATABASE_URL").expect("missing DATABASE_URL environment variable");
+    let config = AsyncDieselConnectionManager::<AsyncPgConnection>::new(db_url);
+    let connection = Pool::builder()
+        .build(config)
+        .await
+        .expect("unable to establish the database connection");
+
+    // Set up the API routes
+    let posts_api = Router::new()
+        .route("/", get(list_posts).post(create_post))
+        .route("/:id", get(get_post).delete(delete_post));
     let app = Router::new()
-        .route("/", get(root))
-        .route("/foo", get(get_foo).post(post_foo))
-        .route("/foo/:name", post(post_foo_name))
-        .route("/parameters", get(get_parameters))
-        .route("/health/", get(health_check));
+        .nest("/posts", posts_api)
+        .with_state(connection);
 
     run(app).await
 }
